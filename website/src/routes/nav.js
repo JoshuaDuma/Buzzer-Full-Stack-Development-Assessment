2eac948f82fe85476941c774da71d3469ff7d27f
JoshuaDuma <Joshua@dumahomes.com>
2022-01-16 00:17:52 -0500

    Final
    Signed-off-by: JoshuaDuma <Joshua@dumahomes.com>

--- website/src/routes/nav.js
+++ website/src/routes/nav.js
@@ -12,7 +12,7 @@ export default function nav() {
                     <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
                         <li><Link className="nav-link px-2 link-dark" to="/">Home</Link></li>
                         <li><Link className="nav-link px-2 link-dark" to="/user_post_summary">User Post Summary</Link></li>
-                        <li><a className="nav-link px-2 link-dark" href="https://github.com/JoshuaDuma/Buzzer---Full-Stack-Development-Assessment">GitHub</a></li>
+                        <li><a className="nav-link px-2 link-dark" href="https://github.com/JoshuaDuma/Buzzer-Full-Stack-Development-Assessment">GitHub</a></li>
                     </ul>
                 </header>
             </div>
